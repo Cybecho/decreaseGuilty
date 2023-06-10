2c1c4d8de7c352c88dde177cb1baebd1dfa25b11
Cybecho_Proxy <qnrwnd10119@gmail.com>
2023-06-10 19:26:23 +0900

    .

--- #GCP/autoWriteREADME.sh
+++ #GCP/autoWriteREADME.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/ubuntu/.bashrc
-cd /home/ubuntu/decreaseGuilty/
+source /home/thquddnr123/.bashrc
+cd /home/thquddnr123/AutoCommit/decreaseGuilty/
 echo "$(date) - $(openssl rand -hex 5) $(echo -e "<br/>")">> ./README.md
 
--- #GCP/commit.sh
+++ #GCP/commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/decreaseGuilty
+cd /home/thquddnr123/AutoCommit/decreaseGuilty
 git add .
 git commit -m "Automated commit $(date)"
 git push origin main
